79eb811a5c23abd9ed29fd72c25325a1a306cb8f
zhuanget <zhuangetwww@163.com>
2020-12-31 10:31:13 +0800

    add: first commit

---
+++ src/main/java/com/zhuanget/wavelengthexplore/WavelengthExploreApplication.java
@@ -0,0 +1,13 @@
+package com.zhuanget.wavelengthexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavelengthExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavelengthExploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhuanget/wavelengthexplore/WavelengthExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuanget.wavelengthexplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavelengthExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
